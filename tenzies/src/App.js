2e66f911460409b844f881fca1bee31ea89c5f38
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-05-26 11:05:48 +0100

    Completed

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,33 +1,88 @@
-import './App.css';
-import Die from "./Die"
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import Die from "./Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+import "./App.css"
 
-function App() {
+export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
 
-  const [dice, setDice] = useState(allNewDice())
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every(die => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
 
-  function allNewDice() {
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
+
+function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push(Math.ceil(Math.random() * 6))
+        newDice.push(generateNewDie())
     }
     return newDice
 }
 
+/**
+* Challenge: Allow the user to play a new game when the
+* button is clicked and they've already won
+*/
+
 function rollDice() {
-    setDice(allNewDice())
+    if(!tenzies) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
+    } else {
+        setTenzies(false)
+        setDice(allNewDice())
+    }
 }
 
-const diceElements = dice.map(die => <Die value={die} />)
+function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
+}
+
+const diceElements = dice.map(die => (
+    <Die 
+        key={die.id} 
+        value={die.value} 
+        isHeld={die.isHeld} 
+        holdDice={() => holdDice(die.id)}
+    />
+))
 
 return (
     <main>
+        {tenzies && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.</p>
         <div className="dice-container">
             {diceElements}
         </div>
-        <button className="roll-dice" onClick={rollDice}>Roll</button>
+        <button 
+            className="roll-dice" 
+            onClick={rollDice}
+        >
+            {tenzies ? "New Game" : "Roll"}
+        </button>
     </main>
 )
 }
-
-export default App;
